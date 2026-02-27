bc8341fe05c606306f0fa9960e9972f9c86f239a
Sigurd Ofstad <sigurd@yne.no>
2026-02-27 09:15:08 +0100

    First commit

---
+++ src/main/java/no/yne/respond/RespondApplication.java
@@ -0,0 +1,13 @@
+package no.yne.respond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RespondApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/yne/respond/RespondApplicationTests.java
@@ -0,0 +1,13 @@
+package no.yne.respond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RespondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
